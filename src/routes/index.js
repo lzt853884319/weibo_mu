d020b862666aa436dbffe71cf54feb026b7be771
lzt <853884319@qq.com>
2020-05-01 10:32:46 +0800

    feature 配置ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,20 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    name: "drdp",
+    isMe: true,
+    blogList: [{
+      id: 1,
+      title: "aaaa",
+    },{
+      id: 2,
+      title: "bbbb",
+    },
+    {
+      id: 1,
+      title: "cccc",
+    }]
   })
 })
 
