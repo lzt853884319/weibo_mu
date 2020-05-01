6c53ddb529a20afe0b03eb84c565cba8c9bf2f41
lzt <853884319@qq.com>
2020-05-01 10:04:24 +0800

    feature 2-4 router的演示

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,30 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.get('/profile/:username', async (ctx, next) => {
+  const {username} = ctx.params;
+  ctx.body = {
+    title: 'this is profile',
+    username
+  }
+})
+router.get('/:loadMore/:pageIndex', async (ctx, next) => {
+  const {username, pageIndex} = ctx.params;
+  ctx.body = {
+    title: `this is loadMore APi`,
+    username,
+    pageIndex
+  }
+})
+
+router.post('/login', async (ctx, next) => {
+  const {username, password  } = ctx.request.body;
+  ctx.body = {
+    title: `this is login`,
+    username,
+    passworld
+  }
+})
+
+
 module.exports = router
